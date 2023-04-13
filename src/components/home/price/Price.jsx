bb7ced63264ce76ef733a9ce6d3c21518e7bbdcf
ZiyovuddinTolipov <ziyovuddinweb@gmail.com>
2023-04-13 15:05:02 +0500

    Price

--- src/components/home/price/Price.jsx
+++ src/components/home/price/Price.jsx
@@ -1,5 +1,5 @@
 import React,{ useState, useEffect } from "react"
-import { price } from "../../assets/data/data"
+// import { price } from "../../assets/data/data" 
 import { Heading } from "../../common/Heading"
 
 export const Price = () => {
@@ -22,9 +22,9 @@ export const Price = () => {
         <Heading title='Choose The Plans' desc='Meet our newbies! The latest templates uploaded to the marketplace.' />
 
         <div className='content'>
-          {price.map((item) => (
+          {posts.map((item) => (
             <div className='box' key={item.id}>
-              <h3>{item.name} LLALLAAAAAAAAAAAAa</h3>
+              <h3>{item.name} </h3>
               <h1>
                 <span>$</span>
                 {item.price}
